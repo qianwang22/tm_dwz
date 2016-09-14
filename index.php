3da197d3e9b2f26f95de3cce1e735269eb93162c
dingwei <827400818@qq.com>
2016-09-14 11:03:04 +0800

    创建项目

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// 应用前台入口文件
+// 检测PHP环境
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG',True);
+
+// 定义应用目录
+define('APP_PATH','./App/');
+
+// 绑定Admin模块到当前入口文件
+define('BIND_MODULE','Web');
+
+// 引入ThinkPHP入口文件
+require './ThinkPHP/ThinkPHP.php';
